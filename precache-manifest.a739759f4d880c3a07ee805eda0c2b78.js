8cecc0b31fdcc0b48a86abdc93ed3c53ea5d30da
Meryem Barroug <barrougmeryem@gmail.com>
2020-10-13 20:56:16 +0200

    Updates

---
+++ precache-manifest.a739759f4d880c3a07ee805eda0c2b78.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f322412354e7bae8653ad5476ee3983c",
+    "url": "/Huddle/index.html"
+  },
+  {
+    "revision": "868fbe40139311ac3d3d",
+    "url": "/Huddle/static/css/main.c8c766de.chunk.css"
+  },
+  {
+    "revision": "0c88c07884bf9b3228a8",
+    "url": "/Huddle/static/js/2.767a9482.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Huddle/static/js/2.767a9482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868fbe40139311ac3d3d",
+    "url": "/Huddle/static/js/main.6c743c80.chunk.js"
+  },
+  {
+    "revision": "0c891bcc0a9e1db6bfa4",
+    "url": "/Huddle/static/js/runtime-main.93d00afb.js"
+  },
+  {
+    "revision": "5b214dd4ae5263c0c58e1c351c6b4c03",
+    "url": "/Huddle/static/media/bg-hero-desktop.5b214dd4.svg"
+  },
+  {
+    "revision": "db68da702e9782cdab4b74d30b0f72d0",
+    "url": "/Huddle/static/media/bg-hero-mobile.db68da70.svg"
+  },
+  {
+    "revision": "a11eb54c16cf590b3b4ba9b436a04d3a",
+    "url": "/Huddle/static/media/icon-email.a11eb54c.svg"
+  },
+  {
+    "revision": "4f06e7d5a51a2ceeb5c20dd6d0651706",
+    "url": "/Huddle/static/media/icon-location.4f06e7d5.svg"
+  },
+  {
+    "revision": "d449ff0d0fabd81cac41c1b25e491ccf",
+    "url": "/Huddle/static/media/icon-phone.d449ff0d.svg"
+  },
+  {
+    "revision": "6641e825f006d463db7028607e3ee994",
+    "url": "/Huddle/static/media/illustration-flowing-conversation.6641e825.svg"
+  },
+  {
+    "revision": "bb27da81d1984faa5ef57668534ff1a5",
+    "url": "/Huddle/static/media/illustration-grow-together.bb27da81.svg"
+  },
+  {
+    "revision": "88090acec5276438b135a468364edf12",
+    "url": "/Huddle/static/media/illustration-mockups.88090ace.svg"
+  },
+  {
+    "revision": "8a5af9952fb5ed23779d589621b108a5",
+    "url": "/Huddle/static/media/illustration-your-users.8a5af995.svg"
+  },
+  {
+    "revision": "3cb867305e954abaa1a8b0dd7a4e7c06",
+    "url": "/Huddle/static/media/logo.3cb86730.svg"
+  }
+]);
